8b51748c8e683675d24442d12a8a9b9397370823
Niki952 Niki <nikolamodzelewskaa@wp.pl>
2024-12-02 17:51:43 +0000

    Created msg storage

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,24 @@
+use::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk_::query]
+fn get_msg() -> String{
+    MSG.with(|msg| msg.borrow().clone())
+}
+
+#[ic_cdk_::update]
+fn get_msg(new_msg) -> String{
+    MSG.with(|msg| *msg.borrow_mut() = new_msg)
+}
+
+//COUNTER.with(|count| *count.borrow_mut() = n);
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+ 
